2bdac7eae399216eb7553f6755fcd0b01b2d3a71
Francisco Igor <franciscoigor@gmail.com>
2018-03-30 20:43:57 -0700

    Use default names for fonts

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,7 +1,7 @@
 
 export default
-function imageIcon(obj){
-	var img=document.createElement("img");
-	img.src=obj;
+	function imageIcon(obj) {
+	var img = document.createElement("img");
+	img.src = obj;
 	return img;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webPath = 'www';
 
 module.exports = {
-  entry:{
-	app:'./src/index.js',
+  entry: {
+    app: './src/index.js',
   },
   devtool: 'inline-source-map',
   output: {
@@ -15,47 +15,52 @@ module.exports = {
     path: path.resolve(__dirname, webPath)
   },
   plugins: [
-	  new CleanWebpackPlugin([webPath]),
+    new CleanWebpackPlugin([webPath]),
     new HtmlWebpackPlugin({
       title: 'WebPack Template',
-  	  template: 'src/index.html',
-      hash:true,
-  	  description: 'Webpack template with basic components and setup',
-  	  keywords: 'Webpack, Template, Node.js, npm',
-  	  website: 'https://fraigo.github.io/webpack_template/dist/',
-  	  logo:'icon.png',
-  	  logoWidth:256,
-  	  logoHeight:256
+      template: 'src/index.html',
+      hash: true,
+      description: 'Webpack template with basic components and setup',
+      keywords: 'Webpack, Template, Node.js, npm',
+      website: 'https://fraigo.github.io/webpack_template/dist/',
+      logo: 'icon.png',
+      logoWidth: 256,
+      logoHeight: 256
     }),
     new UglifyJSPlugin()
   ],
   module: {
-	rules: [
-	 {
-		test: /\.css$/,
-		use: [
-		'style-loader',
-		'css-loader'
-		]
-	 },
-	 {
-         test: /\.(png|svg|jpg|gif)$/,
-         use: [
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
           {
             loader: 'file-loader',
             options: {
-				name: '[name].[ext]'
-			  } 
+              name: '[name].[ext]'
+            }
           }
         ]
-     },
-	 {
-         test: /\.(woff|woff2|eot|ttf|otf)$/,
-         use: [
-           'file-loader'
-         ]
-     },
-	 {
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
         test: /\.(csv|tsv)$/,
         use: [
           'csv-loader'
@@ -67,6 +72,6 @@ module.exports = {
           'xml-loader'
         ]
       }
-	]
+    ]
   }
 };
